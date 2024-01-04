7823deba6e567446c4e9cb002885201042ee0468
isaac <142614504+isaacdemelorocha@users.noreply.github.com>
2024-01-03 22:18:48 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function Mudarestado1(el) {
+    var display = document.getElementById(el).style.display;
+    if(display == "block")
+        document.getElementById(el).style.display = 'none';
+    else
+        document.getElementById(el).style.display = 'block';
+}
+
+
+function Mudarestado2(el) {
+    var display = document.getElementById(el).style.display;
+    if(display == "block")
+        document.getElementById(el).style.display = 'none';
+    else
+        document.getElementById(el).style.display = 'block';
+
+}
+
+function Mudarestado3(el) {
+    var display = document.getElementById(el).style.display;
+    if(display == "block")
+        document.getElementById(el).style.display = 'none';
+    else
+        document.getElementById(el).style.display = 'block';
+
+}
